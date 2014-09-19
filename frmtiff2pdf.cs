feda8b58989aa8d4e7f842457c28ba620d2c8c95
trentonwallace <trentonwallace@yahoo.com>
2014-09-19 08:41:10 -0500

    Update frmtiff2pdf.cs
    cleaned up comments

--- frmtiff2pdf.cs
+++ frmtiff2pdf.cs
@@ -41,8 +41,6 @@ namespace tiffTopdf
                 {
                     try
                     {
-                        //File.Delete(fileName);
-                        //string destFile = Path.GetDirectoryName(pdfFile);
                         string destFile = Environment.CurrentDirectory + "\\data\\";
                         destFile = destFile.Substring(0, destFile.LastIndexOf('\\') + 1);
                         if (Directory.Exists(destFile))
@@ -82,19 +80,11 @@ namespace tiffTopdf
             iTextSharp.text.Rectangle pgSize = PageSize.LEGAL;
             string pdfFile;
 
-            /* pdfFile = Application.StartupPath + "\\" + 
-                Path.GetFileNameWithoutExtension(tempImageFileName) + ".pdf"; */
-            //pdfFile = String.Format("{0}\\temp\\{1}.pdf",
-            //    Application.StartupPath,
-            //    Path.GetFileNameWithoutExtension(tempImageFileName));
-            //pdfFile = tiffFileName.Substring(0, tiffFileName.LastIndexOf('.')) + ".pdf";
             do
             {
                 pdfFile = Path.GetDirectoryName(tiffFileName) + Path.DirectorySeparatorChar +
                     "Signed_Investor_AFE_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".pdf";
             } while (File.Exists(pdfFile));
-            //MessageBox.Show(pdfFile);
-            //return null;
 
             if (File.Exists(pdfFile))
                 File.Delete(pdfFile);
@@ -102,35 +92,14 @@ namespace tiffTopdf
             if (!File.Exists(tiffFileName))
                 return null;
 
-            // option 1
             Bitmap bmp = new Bitmap(tiffFileName);
-            // option 2
-            //System.Drawing.Image bmp2 = Bitmap.FromFile(tiffFileName);
-            //Bitmap bmp2 = new Bitmap(tiffFileName);
-            //Bitmap bmp = new Bitmap(bmp2);
-            //bmp2.Dispose();
-            //bmp.Save("c:\temp\test2.tif");
-            // option 3
-            //System.IO.FileStream strm;
-            //strm = new System.IO.FileStream(tiffFileName, FileMode.Open);
-            ////pic1.Image = Image.FromStream(strm);
-            //Bitmap bmp = new Bitmap(strm);
-            //strm.Close();
 
-            //iTextSharp.text.Image bmp = iTextSharp.text.Image.GetInstance(tiffFileName);
-            //System.Drawing.Image bmp = System.Drawing.Image.FromFile(tempImageFileName);
             int totalPages = bmp.GetFrameCount(FrameDimension.Page);
-            //MessageBox.Show(bmp.Height.ToString());
-            //if (bmp.Height > 3300)
-            //if (bmp.Height > 3399)
-            //    pgSize = PageSize.LEGAL;
 
             // creation of the document with a certain size and certain margins
             float margin = 0.0f;
             //float margin = 50.0f;
             Document document = new Document(pgSize, margin, margin, margin, margin);
-            //Document.Compress = false;
-            //Document.Compress = true;
 
             try
             {
@@ -138,9 +107,6 @@ namespace tiffTopdf
                 // creation of the different writers
                 PdfWriter writer = PdfWriter.GetInstance(document,
                     new FileStream(pdfFile, FileMode.Create));
-
-                //Console.WriteLine("Number of images in this TIFF: " + totalPages);
-                //MessageBox.Show(this, "Number of images in this TIFF: " + totalPages.ToString());
 
                 // Which of the multiple images in the TIFF file do we want to load
                 // 0 refers to the first, 1 to the second and so on.
@@ -151,24 +117,9 @@ namespace tiffTopdf
                 for (int ii = 0; ii < totalPages; ii++)
                 {
                     bmp.SelectActiveFrame(FrameDimension.Page, ii);
-                    //iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(bmp, null, true);
-                    //iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(bmp, null, false);
-
                     img = iTextSharp.text.Image.GetInstance(BitmapToBytes(bmp));
-
-                    //System.IO.MemoryStream stream = new System.IO.MemoryStream();
-                    //bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                    //iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(stream.ToArray());
-                    //stream.Close();
-                    //stream.Dispose();
-
-                    //iTextSharp.text.Image img = System.Drawing.Image.FromFile(tempImageFileName);
-                    //System.Drawing.Image.FromFile()
-                    //iTextSharp.text.pdf.codec.TiffImage.GetTiffImage(RandomAccessFileOrArray, int Page);
-                    //img.ScalePercent(72f / 200f * 100);
                     img.ScalePercent(72f / bmp.HorizontalResolution * 100);
                     img.SetAbsolutePosition(0, 0);
-                    //Console.WriteLine("Image: " + ii);
                     cb.AddImage(img);
                     document.NewPage();
                 }
@@ -183,8 +134,6 @@ namespace tiffTopdf
             {
                 this.Cursor = Cursors.Default;
                 MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                //Console.Error.WriteLine(ex.Message);
-                //Console.Error.WriteLine(ex.StackTrace);
                 return null;
             }
 
@@ -196,7 +145,6 @@ namespace tiffTopdf
             byte[] data = new byte[0];
             using (MemoryStream ms = new MemoryStream())
             {
-                //bmp.Save(ms, ImageFormat.Tiff);
                 bmp.Save(ms, ImageFormat.Png);
                 ms.Seek(0, 0);
                 data = ms.ToArray();
